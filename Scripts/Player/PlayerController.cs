c31acfc05000c84478de59f169f5a96c0d9a0180
Ismail Yildirim <145703561+iskoto@users.noreply.github.com>
2026-09-11 23:36:42 +0300

    Add files via upload

---
+++ Scripts/Player/PlayerController.cs
@@ -0,0 +1,154 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Movement")] public float moveSpeed;
+    private Vector2 currentMovementInput;
+    public float jumpForce;
+    public LayerMask groundLayerMask;
+
+    [Header("Look")] public Transform cameraContainer;
+    public float minXLook;
+    public float maxXLook;
+    private float camCurXRot;
+    public float lookSensitivity;
+
+    private Vector2 mouseDelta;
+
+    [HideInInspector]
+    public bool canLook=true;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    // Start is called before the first frame update
+
+    //components
+
+    private Rigidbody rig;
+    public static PlayerController instance;
+    private void Awake()
+    {
+        rig = GetComponent<Rigidbody>();
+
+        instance = this;
+    }
+
+
+
+    //fixeduptade The reason I use it is to make the physics work better by running in 0.02 seconds and not every frame.
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void LateUpdate()
+    {
+        if (canLook == true)
+        {
+            CameraLook();
+        }
+        
+    }
+
+    private void Move()
+    {
+        Vector3 dir = transform.forward * currentMovementInput.y + transform.right * currentMovementInput.x;
+        dir *= moveSpeed;
+        dir.y = rig.velocity.y;
+
+        rig.velocity = dir;
+
+    }
+
+    private void CameraLook()
+    {
+        //look up
+        camCurXRot += mouseDelta.y * lookSensitivity;
+        //clamp the max and min look
+        camCurXRot = Mathf.Clamp(camCurXRot, minXLook, maxXLook);
+        cameraContainer.localEulerAngles = new Vector3(-camCurXRot, 0, 0);
+        transform.eulerAngles += new Vector3(0, mouseDelta.x * lookSensitivity, 0);
+
+    }
+
+    public void OnLookInput(InputAction.CallbackContext context)
+    {
+        mouseDelta = context.ReadValue<Vector2>();
+    }
+
+    public void OnMoveInput(InputAction.CallbackContext context)
+    {
+        //WASD movement.
+        if (context.phase == InputActionPhase.Performed)
+        {
+            //will walk in the specified direction...
+            currentMovementInput = context.ReadValue<Vector2>();
+        }
+        //if we leave space
+        else if (context.phase == InputActionPhase.Canceled)
+        {   
+            //will stop
+            currentMovementInput = Vector2.zero;
+        }
+    }
+
+    public void OnJumpInput(InputAction.CallbackContext context)
+    {
+        //if we press space..
+        if (context.phase == InputActionPhase.Started)
+        {
+            //Are we on the ground? ground layer
+            if (isGrounded())
+            {
+                //jump
+                rig.AddForce(Vector3.up*jumpForce, ForceMode.Impulse);
+                
+            }
+        }
+    }
+
+    bool isGrounded()
+    {
+        Ray[] rays = new Ray[4]
+        {
+            new Ray (transform.position + (transform.forward * 0.2f)+(Vector3.up*0.01f),Vector3.down),
+            new Ray(transform.position + (-transform.forward * 0.2f)+(Vector3.up*0.01f),Vector3.down),
+            new Ray(transform.position + (transform.right * 0.2f)+(Vector3.up*0.01f),Vector3.down),
+            new Ray(transform.position + (-transform.right * 0.2f)+(Vector3.up*0.01f),Vector3.down)
+
+        };
+        for (int i = 0; i < rays.Length; i++)
+        {
+            if (Physics.Raycast(rays[i], 0.1f, groundLayerMask))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position + (transform.forward * 0.2f), Vector3.down);
+        Gizmos.DrawRay(transform.position + (-transform.forward * 0.2f),Vector3.down);
+        Gizmos.DrawRay(transform.position + (transform.right * 0.2f),Vector3.down);
+        Gizmos.DrawRay(transform.position + (-transform.right * 0.2f),Vector3.down);
+        
+    }
+
+    public void ToggleCursor(bool toggle)
+    {
+        //if the cursor is locked set it to none otherwise set the cursor to lock mode
+        Cursor.lockState = toggle ? CursorLockMode.None : CursorLockMode.Locked;
+        //can look set to opposite of toggle
+        canLook = !toggle;
+    }
+}
---
+++ Scripts/Player/PlayerNeeds.cs
@@ -0,0 +1,153 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+
+
+public class PlayerNeeds : MonoBehaviour,IDamagable
+
+{
+    public Image FadeScreen4;
+    
+    public Need health;
+    public Need hunger;
+    public Need thirst;
+    public Need sleep;
+
+    public float hungerHealthdecay;
+    public float thirstHealthdecay;
+
+    public UnityEvent onTakeDamage;
+
+    public static PlayerNeeds instance;
+
+    //singleton
+    void Awake()
+    {
+        instance = this;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        //initial values ​​at the beginning of the game  (currentValue = startValue)
+        health.currentValue = health.startValue;
+        hunger.currentValue = hunger.startValue;
+        thirst.currentValue = thirst.startValue;
+        sleep.currentValue = sleep.startValue;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Change in bars over time
+        hunger.Subtrack(hunger.decayRate*Time.deltaTime);
+        thirst.Subtrack(thirst.decayRate*Time.deltaTime);
+        sleep.Subtrack(sleep.regenrate*Time.deltaTime);
+
+        //Health bar decreases when hungry
+        if (hunger.currentValue == 0.0f)
+        {
+            health.Subtrack(hungerHealthdecay * Time.deltaTime);
+        }
+
+        //when thirsty the life bar decreases
+        if (thirst.currentValue == 0.0f)
+        {
+            health.Subtrack(thirstHealthdecay * Time.deltaTime);
+        }
+
+        //character death
+        if (health.currentValue == 0.0f)
+        {
+            Die();
+        }
+
+        //Updating UI bars The % value received with GetPercentage is in the UI section.
+        //Will allow bar images to change horizontally.
+        health.uiBar.fillAmount = health.GetPercentage();
+        hunger.uiBar.fillAmount = hunger.GetPercentage();
+        thirst.uiBar.fillAmount = thirst.GetPercentage();
+        sleep.uiBar.fillAmount = sleep.GetPercentage();
+
+    }
+
+    public void Heal(float amount)
+    {
+        health.Add(amount);
+    }
+    public void Eat(float amount)
+    {
+        hunger.Add(amount);
+    }
+    public void Drink(float amount)
+    {
+        thirst.Add(amount);
+    }
+    public void Sleep(float amount)
+    {
+        sleep.Subtrack(amount);
+    }
+    public void TakePhysicDamage(int amount)
+    {
+        health.Subtrack(amount);
+        onTakeDamage?.Invoke();  //? --> if onTakeDamage event occurs
+                                 //.invoke executes the method
+    }
+
+    public void Die()
+    {
+        FadeScreen4.GetComponent<Animation>().Play("you_died");
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+        SceneManager.LoadScene("Menu");
+        
+    }
+    
+    
+    
+    
+    
+}
+
+[System.Serializable]
+public class Need
+{   
+    [HideInInspector]
+    public float currentValue;
+    public float maxValue;
+    public float startValue;
+    public float regenrate;
+    public float decayRate;
+    public Image uiBar;
+
+    // The Add and Subtrack methods are located under the Needs tab.
+    // Determines the max and min points of hp, energy, thirst and sleep bars..
+    public void Add(float amount)
+    {
+        currentValue = Mathf.Min(currentValue + amount, maxValue);
+    }
+
+    public void Subtrack(float amount)
+    {
+        currentValue = Mathf.Max(currentValue - amount, 0);
+    }
+    // Gives the percentage of the bar
+    public float GetPercentage()
+    {
+        return currentValue / maxValue;
+    }
+}
+
+public interface IDamagable
+{
+    void TakePhysicDamage(int damageAmount);
+}
